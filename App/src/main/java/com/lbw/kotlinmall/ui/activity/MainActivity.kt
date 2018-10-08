8a5f84ea8798dfeeabd8d3010f835daddf1c82b8
LiBW <704836663@qq.com>
2018-10-08 13:39:50 +0800

    第一次提交

---
+++ App/src/main/java/com/lbw/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lbw.kotlinmall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lbw.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
