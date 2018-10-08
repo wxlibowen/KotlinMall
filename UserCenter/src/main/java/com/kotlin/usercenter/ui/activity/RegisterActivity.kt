948f431e0ddc9464433b749ba1aad8303db0719e
LiBW <704836663@qq.com>
2018-10-08 13:41:09 +0800

    测试git

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.usercenter.R
 
+/**
+ * 登录页面
+ */
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
